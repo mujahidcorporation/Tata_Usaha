88b41b8d6247a9b6448d77fb319e258a6a444dab
Muhammad Mujahidin Eko Prasetyo <158280679+mujahidcorporation@users.noreply.github.com>
2026-05-12 17:03:20 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,3 +10,37 @@ function showMenu(id){
     .classList.add("active");
 
 }
+
+function searchTable(inputId, frameId){
+
+  let input = document.getElementById(inputId).value.toLowerCase();
+
+  let iframe = document.getElementById(frameId);
+
+  try{
+
+    let innerDoc = iframe.contentWindow.document;
+
+    let rows = innerDoc.querySelectorAll("table tr");
+
+    rows.forEach((row,index)=>{
+
+      if(index === 0) return;
+
+      let text = row.innerText.toLowerCase();
+
+      if(text.includes(input)){
+        row.style.display = "";
+      }else{
+        row.style.display = "none";
+      }
+
+    });
+
+  }catch(err){
+
+    console.log("Pencarian iframe dibatasi browser");
+
+  }
+
+}
